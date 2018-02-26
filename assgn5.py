0f4c3d01d8c079a0505d059dad5eacd02e8685ab
Max Korsun <mkorsun@stuy.edu>
2018-02-25 20:53:29 -0500

    done

--- assgn5.py
+++ assgn5.py
@@ -1,13 +1,45 @@
-import pymongo
+from pymongo import MongoClient
+import json
+
+#Our connection method works thus
+#connect to homer
+#create a db called PoGoDex
+#make a collection called pokemon
+
+#Data is already a list of dictionaries, so we can directly do insert_many with it
+
 
 connection = pymongo.MongoClient("homer.stuy.edu")
-pokedex = connection.PoGoDex
+db = connection.PoGoDex
+collection = db.pokemon
 filename = "PoGoDex.json"
-filename = open(filename,'r')
-
-for x in filename:
-    print x
-    break
-
+filename = open(filename)
+file = filename.read()
+data = json.loads(file)
 filename.close()
+data = data['pokemon']
+
+collection.insert_many(data)
+
+def find(pokemon):
+    return collection.find({"name" : pokemon })
+
+def findid(ID):
+    return collection.find({ "id" : ID})
+
+def findevo(evo):
+    return collection.find({ "next_evolution.name"  :evo })
+
+
+    
+
+
+
+
+
+
+
+
+
+
     
